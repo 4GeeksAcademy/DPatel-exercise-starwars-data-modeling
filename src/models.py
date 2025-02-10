b95ef0fe7bbe62e634cf33441933864948da3a9c
22devkpatel <22devkpatel@gmail.com>
2025-02-10 23:05:02 +0000

    class's

--- src/models.py
+++ src/models.py
@@ -7,23 +7,32 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class People(Base):
+    __tablename__ = 'people'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    gender = Column(String(250))
+    height = Column(String(250))
+    homeworld = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    Cost = Column(String(250), nullable=False)
+    Manufacturer = Column(String(250), nullable=False)
+    Model = Column(String(250))
+    length = Column(String(250))
+    Created = Column(String(250))
+
+class Species(Base):
+    __tablename__ = 'Species'
+    id = Column(Integer, primary_key=True)
+    Name = Column(String(250), nullable=False)
+    Homeworld = Column(String(250), nullable=False)
+    Language = Column(String(250),)
+    Average_Lifespan = Column(String(250))
+    Classification = Column(String(250))
 
     def to_dict(self):
         return {}
